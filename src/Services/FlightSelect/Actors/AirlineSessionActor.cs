f93323e29a7b772a1e73e2fc17b42d82ca0fa2c6
Joe Pemberton <jpemberton@consultantjoes.com>
2024-03-19 21:21:41 -0600

    Started implementing Dapr Actors

---
+++ src/Services/FlightSelect/Actors/AirlineSessionActor.cs
@@ -0,0 +1,22 @@
+﻿using Dapr.Actors.Runtime;
+using FlightSelect.Models;
+
+namespace FlightSelect.Actors;
+
+public class AirlineSessionActor : Actor, IAirlineSessionActor
+{
+    public AirlineSessionActor(ActorHost host)
+        : base(host)
+    {
+    }
+
+    public Task AddFlightToSession(BookFlightPayload payload)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<BookFlightPayload> GetFlightFromSession()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Services/FlightSelect/Actors/IAirlineSessionActor.cs
@@ -0,0 +1,10 @@
+﻿using Dapr.Actors;
+using FlightSelect.Models;
+
+namespace FlightSelect.Actors;
+
+public interface IAirlineSessionActor : IActor
+{
+    Task<BookFlightPayload> GetFlightFromSession();
+    Task AddFlightToSession(BookFlightPayload payload);
+}
